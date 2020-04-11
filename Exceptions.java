f55215b6deaf7f5566b5256b7bf0efe51de020c7
patrickmbasco <59862023+patrickmbasco@users.noreply.github.com>
2020-04-11 12:41:01 -0700

    Add files via upload

---
+++ Exceptions.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Exceptions {
+    public ArrayList<Object> testing(){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        return myList;
+    }
+
+}
---
+++ ExceptionsTest.java
@@ -0,0 +1,7 @@
+
+public class ExceptionsTest {
+    public static void main(String[] args){
+        Exceptions iD = new Exceptions();
+        System.out.println(iD.testing());
+    }
+}
